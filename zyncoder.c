633c55f537e57bd05cbc779970796f58a31e378e
smiths73v3 <smiths.73v3@gmail.com>
2025-09-13 16:23:21 -0400

    Better Dummy isolation
    Make Dummy skip all hardware gpio dependency. This helps out on
    amd64 systems where gpio support is incomplete, and unused.

--- zyncoder.c
+++ zyncoder.c
@@ -178,6 +178,7 @@ int setup_zynswitch(uint8_t i, uint16_t pin, uint8_t off_state) {
 		if (off_state) zsw->off_state = 1;
 		else zsw->off_state = 0;
 
+#       if !defined( DUMMY_ENCODERS ) && !defined( EMULATOR_ENCODERS )
 		// RBPi GPIO pin
 		if (pin<100) {
 			struct gpiod_line *line = gpiod_chip_get_line(gpio_chip, pin);
@@ -229,6 +230,8 @@ int setup_zynswitch(uint8_t i, uint16_t pin, uint8_t off_state) {
 				}
 			#endif
 		}
+#       endif // !Dummy Encoders and !Emulator Encoders 	
+
 	}
 	return 0;
 }
@@ -248,6 +251,7 @@ int setup_zynswitch_midi(uint8_t i, enum midi_event_type_enum midi_evt, uint8_t 
 
 	//zsw->last_cvgate_note = -1;
 
+#   if !defined( DUMMY_ENCODERS ) && !defined( EMULATOR_ENCODERS )
 	#ifdef ZYNAPTIK_CONFIG
 	if (midi_evt==CVGATE_OUT_EVENT) {
 		set_pin_mode_zynmcp23017(zsw->pin, PIN_MODE_OUTPUT);
@@ -260,7 +264,7 @@ int setup_zynswitch_midi(uint8_t i, enum midi_event_type_enum midi_evt, uint8_t 
 		zynaptik_setup_gateout(i, midi_evt, midi_chan, midi_num);
 	}
 	#endif
-
+#   endif // !Dummy Encoders and !Emulator Encoders
 	return 1;
 }
 
@@ -534,6 +538,7 @@ int setup_zyncoder(uint8_t i, uint16_t pin_a, uint16_t pin_b) {
 	zcdr->short_history = 0;
 	zcdr->long_history = 0;
 
+#   if !defined( DUMMY_ENCODERS ) && !defined( EMULATOR_ENCODERS )
 	if (pin_a!=pin_b) {
 		// RBPi GPIO pins
 		if (pin_a<100 && pin_b<100) {
@@ -609,6 +614,7 @@ int setup_zyncoder(uint8_t i, uint16_t pin_a, uint16_t pin_b) {
 		fprintf(stderr, "ZynCore->setup_zyncoder(%d, %d, %d): Can't configure zyncoder on a single pin!\n", i, pin_a, pin_b);
 		return 0;
 	}
+#   endif // !Dummy Encoders and !Emulator Encoders	
 	return 0;
 }
 
