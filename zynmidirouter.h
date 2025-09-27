f5c5c48bc594eb096a2a1ab73701f860a613c6b4
smiths73v3 <smiths.73v3@gmail.com>
2025-09-27 11:36:36 -0400

    Multi-Timbral or Active MIDI Channel Default
    Selected by environment variable: ZYNCODER_MULTITIMBRAL_DEFAULT
    Default behavior is Active mode if the environment variable is not
    defined or is FALSE.

--- zynmidirouter.h
+++ zynmidirouter.h
@@ -180,22 +180,10 @@ void reset_midi_filter_cc_map();
 #define FLAG_ZMIP_SYSTEM 16
 #define FLAG_ZMIP_SYSTEM_RT 32
 
-//architecture detection
-#if defined(__x86_64__) || defined(_M_X64)
-#define ZYNMIDI_ARCH_X86_64
-#elif defined(__aarch64__)
-#define ZYNMIDI_ARCH_ARM64
-#elif defined(__arm__) || defined(_M_ARM)
-#define ZYNMIDI_ARCH_ARM
-#elif defined(__i386__) || defined(_M_IX86)
-#define ZYNMIDI_ARCH_X86
-#else
-#define ZYNMIDI_ARCH_UNKNOWN
-#endif
-
 #define ZMIP_DEV_FLAGS_BASE (FLAG_ZMIP_UI|FLAG_ZMIP_FILTER|FLAG_ZMIP_SYSTEM|FLAG_ZMIP_SYSTEM_RT)
 
-#if(defined(ZYNMIDI_ARCH_X86_64) || defined(ZYNMIDI_ARCH_X86))
+//sent in by defining ZYNCODER_MULTITIMBRAL_DEFAULT environment variable
+#if defined(MULTITIMBRAL_DEFAULT) && MULTITIMBRAL_DEFAULT
 #   define ZMIP_DEV_FLAGS (ZMIP_DEV_FLAGS_MULTI_ORIG)
 #else
 #   define ZMIP_DEV_FLAGS (ZMIP_DEV_FLAGS_ORIG|FLAG_ZMIP_ACTIVE_CHAIN) //Active-chain for non-x86_64 architectures
